ce536417aadd492a7a3fb87a0658e0d1b646253e
celestroccayoli <celestetroccayoli@gmail.com>
2021-10-25 17:39:32 -0300

    Agregando Express y métodos al JS

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-// armado de app
+const express = require("express");
+const app = express();
+const path = require("path");
+const miRuta =path.resolve(__dirname, "./public");
+
+app.use (express.static(miRuta));
+
+app.listen(2500, () => { console.log("Servidor corriendo en el puerto 2500"); });
+
+app.get("/", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./views/home.html"))
+});
+
+
+

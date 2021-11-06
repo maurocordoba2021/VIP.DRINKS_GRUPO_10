e6508efaf4fa1c2ac4ba8da5b5ee59586a954bd5
celestroccayoli <celestetroccayoli@gmail.com>
2021-11-06 10:07:52 -0300

    agregué las paginas que vamos a utilizar y a todas les agregué las características principales como background, footer y header

--- app.js
+++ app.js
@@ -5,9 +5,13 @@ const miRuta =path.resolve(__dirname, "./public");
 
 app.use (express.static(miRuta));
 
-app.listen(2500, () => { console.log("Servidor corriendo en el puerto 2500"); });
+app.listen(3000, () => { console.log("Servidor corriendo en el puerto 3000"); });
 
 app.get("/", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./views/home.html"))
+});
+
+app.post("/", (req, res) => {
     res.sendFile(path.resolve(__dirname, "./views/home.html"))
 });
 
@@ -18,7 +22,40 @@ app.get("/register", (req, res) => {
 app.get("/detalle-de-productos", (req, res) =>{
     res.sendFile(path.resolve(__dirname, "./views/detalle-de-productos.html"))
 });
-app.get("/Ingreso", (req, res) =>{
-    res.sendFile(path.resolve(__dirname, "./views/fecha-de-nacimiento.html"))
+app.get("/login", (req, res) =>{
+    res.sendFile(path.resolve(__dirname, "./views/login.html"))
+});
+app.get("/clase15", (req, res) =>{
+    res.sendFile(path.resolve(__dirname, "./views/formulario-clase15.html"))
+});
+app.get("/carrito-de-compras", (req, res) =>{
+    res.sendFile(path.resolve(__dirname, "./views/carrito-de-compras.html"))
+});
+app.get("/productos", (req, res) =>{
+    res.sendFile(path.resolve(__dirname, "./views/listado-de-productos.html"))
+});
+app.get("/contacto", (req, res) =>{
+    res.sendFile(path.resolve(__dirname, "./views/contacto.html"))
+});
+app.get("/nosotros", (req, res) =>{
+    res.sendFile(path.resolve(__dirname, "./views/nosotros.html"))
+});
+app.get("/terminos-y-condiciones", (req, res) =>{
+    res.sendFile(path.resolve(__dirname, "./views/terminos-y-condiciones.html"))
+});
+app.get("/novedades", (req, res) =>{
+    res.sendFile(path.resolve(__dirname, "./views/novelties.html"))
+});
+app.get("/recetas", (req, res) =>{
+    res.sendFile(path.resolve(__dirname, "./views/recipes.html"))
+});
+app.get("/perfil", (req, res) =>{
+    res.sendFile(path.resolve(__dirname, "./views/profile.html"))
+});
+app.get("/registro", (req, res) =>{
+    res.sendFile(path.resolve(__dirname, "./views/registro.html"))
+});
+app.get("/metodos-de-pago", (req, res) =>{
+    res.sendFile(path.resolve(__dirname, "./views/metodos-de-pago.html"))
 });
 

40429152518f2b9139a2b45a68e0b035f43ad570
celestroccayoli <celestetroccayoli@gmail.com>
2021-11-30 15:54:02 -0300

    Retocando css y verificando rutas

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+
 app.use(express.static('public'))
+
+//app.use(express.urlencoded({extended: false}));
+//app.use(express.json());
+//const methodOverride = require('method-override');
+//app.use(methodOverride('_method'));
+/*app.use((req, res, next) =>{
+    res.status(404).render('not-found')
+})*/
+
+
 
 /* Modularizado de rutas */
 const shopCar=require('./routes/shop-car')

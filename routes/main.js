6c28b2e87b72e74adfe555ca2c8f5b3606f7fb83
celestroccayoli <celestetroccayoli@gmail.com>
2022-01-17 13:08:43 -0300

    Here I will start my personal project and showcase my skills. This will then be my letter of introduction.

--- routes/main.js
+++ routes/main.js
@@ -2,6 +2,7 @@ const express=require('express');
 const router=express.Router();
 const path = require("path");
 const mainController =require('../controllers/mainController')
+const carController =require('../controllers/carController')
 
 router.get("/", mainController.filter)
 router.get("/home", mainController.index);

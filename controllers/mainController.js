5abb62369211f61a6b03d060c446bea0a560c61e
maurocordoba2021 <echeverriamauro2017@gmail.com>
2022-01-25 15:12:25 -0300

    controllers para crud con db

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -1,16 +1,22 @@
 const express = require('express');
 const path = require('path');
 const fs = require("fs");
-const dirPath = path.join(__dirname, '../database/products.json');
+ /* const dirPath = path.join(__dirname, '../database/products.json');
 let listProducts = JSON.parse(fs.readFileSync(dirPath, 'utf-8'));
-const homeProducts = listProducts.filter(products => products.luxury == false)
-
-const db = require('../database/models');
+const homeProducts = listProducts.filter(products => products.luxury == false) */
+ 
+const db = require('../src/database/models');
 const sequelize = db.sequelize;
 
 const mainController = {
     listProducts:  (req, res) => {
-        res.render("listProducts", { listProducts: homeProducts});
+        db.Product.findAll()
+        .then(Product=>{
+            res.render("listProducts",{listProducts:Product})
+        })
+        .catch(error=>{
+            console.log (error)
+        })
     },
     recipes: (req, res) =>{
         

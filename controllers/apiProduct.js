a2bd444a58629827e06749f804eb7159c6c8b456
maurocordoba2021 <echeverriamauro2017@gmail.com>
2022-03-20 18:43:07 -0300

    metodo list API product

--- controllers/apiProduct.js
+++ controllers/apiProduct.js
@@ -3,17 +3,88 @@ const sequelize = db.sequelize;
 const { Op } = require("sequelize");
 
 
-apiProduct = {
-    list : ( req , res) => {
-        db.Product.findAll()
-            .then( Products =>{
-                return res.json({
-                    total : Products.length ,
-                    Products:Products
-
-                })
+apiProduct = {list:(req,res)=>{
+    db.Categorie.findAll({
+        include:['categorie_product']
+    })
+    .then(Categorie=> {
+       
+    db.Product.findAll({
+    include:["Categories"]
+})
+    .then(products=>{
+        console.log(products)
+        let arrayCategorias = []
+        for(let i=0; i<Categorie.length ; i++){
+            arrayCategorias.push({
+                nombre: Categorie[i].dataValues.name,
+                total: Categorie[i].dataValues.categorie_product.length
             })
-    }
+        }
+
+        let wisky = products.filter(product => product.categories_idcategories == 1)
+        let tequila = products.filter(product => product.categories_idcategories == 8)
+        let ron = products.filter(product => product.categories_idcategories == 3)
+        let respuesta = {
+            meta: {
+                status: 200,
+                total: products.length,
+                url: "/api/products",
+                categorie: Categorie.length,
+                categoryNames: arrayCategorias,
+                countByCategory: [
+                    {wisky: wisky.length},
+                    {tequila: tequila.length},
+                    {ron: ron.length}
+                ]
+            },
+              data: products.map(product => {
+                console.log(product)
+                return{
+                    idProducts: product.idProducts,
+                    name:product.title,
+                    measure:product.measure,
+                    price:product.price,
+                    short_description: product.short_description,
+                    long_description:product.long_description,
+                    brand:product.brand,
+                    discount: product.discount,
+                    image: "/images/imgProducts/" + product.img,       
+                     Categories:{name:product.Categories.name} 
+                    
+                }
+            })  
+        } 
+    
+        res.json(respuesta)
+    })    
+    })
+    .catch(function(error){
+        res.json({status:800})
+    })
+}
+    
 }
 
-module.exports=apiProduct
+module.exports = apiProduct
+
+
+
+
+
+
+list: (req, res) => {
+    db.Categorie.findAll()
+        .then(Categorie => {
+
+            console.log(Categorie)
+            let array = []
+            for (let i = 0; i < Categorie.length; i++) {
+                array.push({
+                    nombre: Categorie[i].dataValues.name,
+                    total: Categorie[i].dataValues.idcategories
+                })
+            }
+            return res.json({array})
+        }) 
+        }
